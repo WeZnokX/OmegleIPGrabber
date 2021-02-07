c73e8bbe634b4d9c26c9132b9581bb2074635527
WeZnokX <66142968+WeZnokX@users.noreply.github.com>
2021-02-08 00:40:34 +0100

    Update omegleipgrabber.js

--- omegleipgrabber.js
+++ omegleipgrabber.js
@@ -1,39 +1,41 @@
-const apiKey = "Votre clé API ici";
+const apiKey = "Votre clé API ici.";
 
-window.oRTCPeerConnection = 
- window.oRTCPeerConnection || windowRTCPeerConnection;
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
 
-window.RTCPeerCOnnection = function (...args) {
-    const pc = new window.oRTCPeerConnection(...args);
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
 
-    pc.oaddIceCandidate = pc.addIceCandidate;
+  pc.oaddIceCandidate = pc.addIceCandidate;
 
-    pc.addIceCandidate = function (iceCandidate, ...rest) {
-        const fields = iceCandidate.candidate.split(" ");
-        const ip = fields[4];
-        if (fields[7] === "srflx") {
-            getLocation(ip);
-        }
-        return pc.oaddIceCandidate(iceCandidate, ...rest);
-    };
-    return pc;
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    const ip = fields[4];
+
+    if (fields[7] === "srflx") getLocation(ip);
+
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+
+  return pc;
 };
 
 const getLocation = async (ip) => {
-    let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
-
-    await fetch(url).then((response) =>
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+  await fetch(url).then((response) =>
     response.json().then((json) => {
-        const output = `
-        --------------------
-        Pays: ${json.country_name}
-        État: ${json.state_prov}
-        Ville: ${json.city}
-        Arrondissement: ${json.district}
-        Lat / Long: ${json.latitude}, ${json.longitude})
-        --------------------
-        `;
-    console.log(output);
+
+      const output = `
+          ---------------------
+          Pays : ${json.country_name}
+          État : ${json.state_prov}
+          Ville : ${json.city}
+          Arrondissement : ${json.district}
+          Lat / Long : (${json.latitude}, ${json.longitude})
+          ---------------------
+          `;
+      console.log(output);
     })
-    );
+  );
 };
